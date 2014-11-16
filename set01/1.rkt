3dd42d8612e12fb391783b948b3d1de68f40cceb
zgs <zhengguanshi@gmail.com>
2014-11-16 10:24:42 +0800

    Begin set01 q1

---
+++ set01/1.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(require rackunit)
+(require "extras.rkt")
+
+(provide
+  initial-robot
+  robot-left 
+  robot-right
+  robot-forward
+  robot-north? 
+  robot-south? 
+  robot-east? 
+  robot-west?)
+
